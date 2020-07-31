4f84d73a35779812b6d7922ac33fdc037b5d1fcb
David Hewitt <davidmhewitt@users.noreply.github.com>
2020-07-31 14:35:39 +0100

    Fix brackets

--- build-pinebookpro.sh
+++ build-pinebookpro.sh
@@ -201,7 +201,7 @@ LANG=C chroot elementary-$architecture /build-initramfs
 # Calculate the space to create the image.
 root_size=$(du -s -B1 ${work_dir} | cut -f1)
 root_extra=$((${root_size}/1024/1000*5*1024/5))
-raw_size=$(($((${free_space}*1024))+${root_extra}
+raw_size=$(($((${free_space}*1024))+${root_extra}))
 
 # Create the disk and partition it
 echo "Creating image file"
